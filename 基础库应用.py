fa589eeb0bc3e5c91270046bfbd6c4b2f0972050
夜看星空 <1749011469@qq.com>
2022-03-16 13:24:42 +0800

    Create 基础库应用.py

---
+++ 基础库应用.py
@@ -0,0 +1,22 @@
+import Mylocalone
+import pymysql
+import pymssql
+import pyodbc
+import os
+import re
+import requests
+import requests_cache
+import requests_html
+import configparser
+import tkinter.messagebox
+import time
+from urllib.parse import unquote,urlencode,quote
+import struct
+import mysql.connector
+import bs4
+import shutil
+import smb.SMBConnection
+from lxml import etree
+BeautifulSoup = bs4.BeautifulSoup()
+ConfigParser = configparser.ConfigParser()
+messagebox = tkinter.messagebox
